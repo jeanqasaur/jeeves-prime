7d4ec8a896b338e2e6adca0c2e788d27af428e6c
Pat <Pat@plap>
2012-03-24 11:13:45 -0400

    Added Tests

--- src/cap/primes/Update.scala
+++ src/cap/primes/Update.scala
@@ -11,9 +11,16 @@ case class Update(private val message: Message, private val author: User) extend
 	
 	private val visible = mkLevel()
 	policy(visible, !(author.isFriends(CONTEXT.asInstanceOf[SocialNetContext].viewer) || 
-			isTagged(CONTEXT.asInstanceOf[SocialNetContext].viewer)), LOW)
+			isTagged(CONTEXT.asInstanceOf[SocialNetContext].viewer) ||
+			CONTEXT.asInstanceOf[SocialNetContext].viewer.hasFriend(tags) ||
+			author.username == CONTEXT.asInstanceOf[SocialNetContext].viewer.username), LOW)
 	
 	def isTagged(user: User) = tags contains user.username
+	def tag(user: Username, editor: Username) = {
+		if(author.username == editor) {
+			tags += user
+		}
+	}
 	
 	def getMessage(): Symbolic = mkSensitive(visible, message, Message("Unauthorized"))
 	def showMessage(ctxt: SocialNetContext): String = concretize(ctxt, getMessage()).asInstanceOf[Message].message
