06360ca1007101d86a835a58af5f6ce6179338d7
Pat <Pat@plap>
2012-04-19 09:36:04 -0400

    updated lib

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 package cap.primes
 
 object Main extends App {
+	System.setProperty("smt.home", "C:\\Program Files (x86)\\Microsoft Research\\Z3\\bin\\z3")
 	var daemon: Server = new Server()
 	daemon.init("socialist.map")
 	daemon.run()
